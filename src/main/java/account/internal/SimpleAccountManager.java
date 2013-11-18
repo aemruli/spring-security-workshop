e9082984122ff0604049382ee1782573676fb78a
Agim Emruli <agimemruli@gmail.com>
2013-11-18 09:35:45 +0100

    Switched to @PreAuthorize annotation

--- src/main/java/account/internal/SimpleAccountManager.java
+++ src/main/java/account/internal/SimpleAccountManager.java
@@ -2,7 +2,7 @@ package account.internal;
 
 import account.AccountManager;
 import account.domain.Account;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -22,14 +22,14 @@ public class SimpleAccountManager implements AccountManager {
         accounts.put("1", new Account("1", "Agim", "Emruli"));
     }
 
-    @Secured("ROLE_USER")
+    @PreAuthorize("hasRole('ROLE_USER')")
     @Override
     public Account getById(String accountId) {
         Account source = accounts.get(accountId);
         return new Account(source.getAccountId(), source.getFirstName(), source.getLastName());
     }
 
-    @Secured({"ROLE_USER","ROLE_ADMIN"})
+    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     @Override
     public void storeAccount(Account account) {
         accounts.put(account.getAccountId(), account);
